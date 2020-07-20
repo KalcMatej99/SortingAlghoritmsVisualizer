bceb048ede1085d7163feb21c8e48d29a850b358
KalcMatej99 <mk9693@student.uni-lj.si>
2020-07-20 10:28:45 +0200

    Added super fast velocity

--- public/javascripts/constants.js
+++ public/javascripts/constants.js
@@ -2,6 +2,7 @@
 const slowVelocitySorting = 15000;
 const mediumVelocitySorting = 10000;
 const fastVelocitySorting = 5000;
+const superFastVelocitySorting = 2000;
 
 const littleSizeArray = 10;
 const middleSizeArray = 100;
